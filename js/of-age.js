276539f129305f68283c1d3f528741c3f48b31f0
Javier <apollo.infinity88@gmail.com>
2017-11-29 11:53:35 -0800

    Adding new files and folders in project

--- js/of-age.js
+++ js/of-age.js
@@ -7,20 +7,20 @@ function printFullAge(years){
         //for loop iterates through the array of numbers
         ages[i] = 2017 - years[i]; // ages[i] is assigned each of the ages from the 2017 - years[i]
     }
-
+    
     //This second loop iterates through the ages array
     for (var j = 0; j < ages.length; j++){
-
-        // The if else checks to see if the ages are over 18 and console log a corresponding message.
+        
+        // The if else checks to see if the ages are over 18 and console log a corresponding message. 
         if(ages[j] >= 18){
             console.log("The person is " + ages[j] + " years old and is of full age");
-
+            
             //If the ages is over 18, it is then pushed to the fullAges array
             fullAges.push(true);
         } else {
             console.log("The person is " + ages[j] + " years old and is not of full age");
-
-            //If it is not over 18, false gets pushed to the array.
+            
+            //If it is not over 18, false gets pushed to the array. 
             fullAges.push(false);
         }
     }

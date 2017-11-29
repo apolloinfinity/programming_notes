91278f3f307d2c560a9e65a7edfc5f38c806cbab
Javier <apollo.infinity88@gmail.com>
2017-11-29 15:09:41 -0800

    Finished my solution, I also added a much better solution using regex

--- js/cdwars_reverseLetter.js
+++ js/cdwars_reverseLetter.js
@@ -21,14 +21,34 @@ Use Slice
 Use Split and Reverse
 */
 
+//This is my solution to the problem. There is a much simpler solution. 
 
-function reverseLetter(str) {
-    var cleanStr = str.replace(/[0-9,!@#$%^&*(){}:"<>?"]/gi, "");
+//function reverseLetter(str) {
+//    var cleanStr = str.replace(/[0-9,\^ \$ \* \{ \} \+ \\ \/ \- \( \) \_ #?!=%]/gi&@, "");
+//
+//    return cleanStr.split("").reverse().join("");
+//
+//}
 
-    return cleanStr.split("").reverse().join("");
 
-}
-
-
+/*
 console.log(reverseLetter("Hello"));
 console.log(reverseLetter("ultr53o?n"));
+console.log(reverseLetter("\\}t+pajs^ynm$syvjak^izqhpj*x+/#{9i83sfp"));
+console.log(reverseLetter("qfmlwykes_j-pto2rttvbyec5}o5bxsyqvozyohkb"));
+console.log(reverseLetter("xvjxwsbq4qlfzqdkaapy-ajebaftewetdhhc_mhwqfjuapxg"));
+console.log(reverseLetter("ualy*dp!ilx!%6w$zr-wwf{z/?zlfuv4=#o4ovm0"));
+*/
+
+
+//This is a much simpler and faster solution to this problem
+function reverseLetter2(str) {
+    return str.replace(/[^a-z]/gi).split("").reverse().join("");
+}
+
+console.log(reverseLetter2("Hello"));
+console.log(reverseLetter2("ultr53o?n"));
+console.log(reverseLetter2("\\}t+pajs^ynm$syvjak^izqhpj*x+/#{9i83sfp"));
+console.log(reverseLetter2("qfmlwykes_j-pto2rttvbyec5}o5bxsyqvozyohkb"));
+console.log(reverseLetter2("xvjxwsbq4qlfzqdkaapy-ajebaftewetdhhc_mhwqfjuapxg"));
+console.log(reverseLetter2("ualy*dp!ilx!%6w$zr-wwf{z/?zlfuv4=#o4ovm0"));

00624922ff18d56851293b9ada641d3c8fefcde4
Javier Ramirez <apollo.infinity88@gmail.com>
2017-12-17 00:56:26 -0800

    Not quite finished but my guessing number program works. I added a counter for how many times it takes a person to guess the number correctly. I need to add an exception in case someone doesn't enter anything or enters an invalid character

--- python-challenges/guesser.py
+++ python-challenges/guesser.py
@@ -1,9 +1,32 @@
 """guesser.py, By Javier Ramirez, 2017-16-12
 This program has the user guess a number between 1 and 100.
 """
+import random
 
 def main():
-    print("Hello world!")
+
+    print("Guess a number between 1 and 100")
+    # random_Number = 35 for debugging only
+    random_Number = random.randint(1,100)
+    found = False  # flag variable to see
+                   #if they guess the right number
+    guess = 1
+
+    while not found: 
+        user_Guess = int(input("Your guess: "))
+        if user_Guess == random_Number:
+            print("You got it!")
+            found = True
+        elif user_Guess > random_Number:
+            print("Guess lower!")
+            guess += 1
+        else:
+            print("Guess higher!")
+            guess += 1
+    
+    print("Thanks for playing! It took you {} tries!".format(guess))
 
 
-print(main())
+
+if __name__ == "__main__":
+    main()

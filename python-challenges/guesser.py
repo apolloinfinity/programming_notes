80e4e6712378f9fcfd732fd2a5b5a454fffeca67
Javier Ramirez <apollo.infinity88@gmail.com>
2017-12-16 23:26:41 -0800

    created a folder for python challenges and other small programs

---
+++ python-challenges/guesser.py
@@ -0,0 +1,9 @@
+"""guesser.py, By Javier Ramirez, 2017-16-12
+This program has the user guess a number between 1 and 100.
+"""
+
+def main():
+    print("Hello world!")
+
+
+print(main())
---
+++ python-challenges/highest_profit.py
@@ -0,0 +1,12 @@
+# Task
+# Write a function that returns both the minimum and maximum number of the given list/array.
+
+
+def min_max(lst):
+  # newArr = list()
+    return lst.sort()
+
+
+myArr = min_max([1, 2, 3, 4, 49, 6])
+
+print(myArr)

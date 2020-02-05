0f07e126b0359fd2d27832b8b844953141d64c5f
Javier Ramirez <apollo.infinity88@gmail.com>
2020-02-04 20:25:41 -0800

    Added a python specific compScience directory

---
+++ compScienceNotes/python/hash tables/hash_phonebook.py
@@ -0,0 +1,30 @@
+"""
+-phone book is going to store 3 simple items
+-name, last name, and phone number
+This is CS fundementals on hash tables
+"""
+
+# Simple version of hash
+
+phonebook = {}  # empty dictionanary to start off with.
+
+phonebook["mom"] = "555-555-001"
+phonebook["dad"] = "555-555-003"
+phonebook["wife"] = "555-555-003"
+
+# print(phonebook)
+
+# Get wife's phone number
+print(phonebook["wife"])
+# end simple hash map
+
+""" Preventing Duplicates """
+# In the example of the phonebook We don't want duplicates
+# We want the number to only show up once and prevent duplicate number's from polluting the hashmap
+
+
+def check_for_dupe_names(name):
+    if(phonebook[name]):
+        print('That number exists!')
+    else:
+        phonebook[name]
